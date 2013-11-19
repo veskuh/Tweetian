69b7c027f338fedaa9ee7f7b19a42b35060a37af
Siteshwar Vashisht <siteshwar@gmail.com>
2013-11-20 00:20:23 +0530

    Removed dependency over widgets module

--- qmlapplicationviewer/qmlapplicationviewer.cpp
+++ qmlapplicationviewer/qmlapplicationviewer.cpp
@@ -168,12 +168,12 @@ void QmlApplicationViewer::showExpanded()
 #endif
 }
 
-QApplication *createApplication(int &argc, char **argv)
+QGuiApplication *createApplication(int &argc, char **argv)
 {
 #ifdef HARMATTAN_BOOSTER
     //return MDeclarativeCache::qApplication(argc, argv);
-    return new QApplication(argc, argv);
+    return new QGuiApplication(argc, argv);
 #else
-    return new QApplication(argc, argv);
+    return new QGuiApplication(argc, argv);
 #endif
 }
